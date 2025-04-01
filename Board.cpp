44776a6e97b1b9555808a7b6fa5c0a688d3a0b54
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-04-01 20:55:07 +0100

    Created Board classes with initial skeleton code

---
+++ Board.cpp
@@ -0,0 +1,33 @@
+#include "Board.h"
+
+void initializeBoard(vector<Crawler*> &bcrawlers) {
+
+}
+
+void displayAllBugs() {
+
+}
+
+void findBugById() {
+
+}
+
+void tapBugBoard() {
+
+}
+
+void displayLifeHistory() {
+
+}
+
+void displayAllCells() {
+
+}
+
+void runSimulation() {
+
+}
+
+void exit() {
+
+}
---
+++ Board.h
@@ -0,0 +1,25 @@
+//
+// Created by Theo Picar on 01/04/2025.
+//
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "Crawler.h"
+
+class Board {
+public:
+    Board();
+    void initializeBoard(vector<Crawler*> &bcrawlers);
+    void displayAllBugs();
+    void findBugById();
+    void tapBugBoard();
+    void displayLifeHistory();
+    void displayAllCells();
+    void runSimulation();
+    void exit();
+};
+
+
+
+#endif //BOARD_H
